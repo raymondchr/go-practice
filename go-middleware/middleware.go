5a3b1592a016e0131fbf6c1355e44cfa44dd8331
Raymond <raycuffed@gmail.com>
2022-09-29 17:49:48 +0700

    Middleware

---
+++ go-middleware/middleware.go
@@ -0,0 +1,40 @@
+package main
+
+import "net/http"
+
+// USERNAME const
+const USERNAME = "batman"
+
+// PASSWORD const
+const PASSWORD = "secret"
+
+// MiddlewareAuth func
+func MiddlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			w.Write([]byte(`something went wrong`))
+			return
+		}
+
+		isValid := (username == USERNAME) && (password == PASSWORD)
+		if !isValid {
+			w.Write([]byte(`wrong username/password`))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// MiddlewareAllowOnlyGET is the middleware to ensure only GET method can be used
+func MiddlewareAllowOnlyGET(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.Write([]byte("Only GET is allowed"))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
